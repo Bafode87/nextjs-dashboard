375a44479c99e1d6fc54fa90063592cafa4df2ba
Bafode KOULIBALY <bafode.koulibaly@legrand.com>
2024-08-02 08:35:15 +0200

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
+import { Inter } from 'next/font/google';
+import '@/app/ui/global.css'
+
+export const inter = Inter({ 
+    subsets: ['latin'],
+    variable: '--font-inter'
+ });
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} antialiased`}>
       <body>{children}</body>
     </html>
   );
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,17 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({ 
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-lusitana' 
+});
+
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.variable} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
